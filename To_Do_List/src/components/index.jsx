6efbd5c03661603a7dcbfa5571bb3945e2bb51a3
anujakatuwal <anujakatywal010@gmail.com>
2025-06-30 00:17:38 +0545

    Add handleDelete function

--- To_Do_List/src/components/index.jsx
+++ To_Do_List/src/components/index.jsx
@@ -11,6 +11,15 @@ const ToDo = () => {
     setInput("");
   }
 
+  function handleEdit(getCurrentIndex) {
+    
+  }
+
+  function handleDelete(getCurrentIndex) {
+    const updatedTasks = tasks.filter((_, index) => index !== getCurrentIndex);
+    setTasks(updatedTasks);
+  }
+
   useEffect(() => {
     const stored = localStorage.getItem("tasks");
     if (stored) setTasks(JSON.parse(stored));
@@ -24,34 +33,36 @@ const ToDo = () => {
   }, [tasks, hasLoaded]);
 
   return (
-    <div className="bg-teal-300 h-screen p-10">
-      <div className="h-[70vh] bg-white rounded-2xl w-[430px] m-auto">
-        <h1 className="font-bold text-2xl">To-Do List</h1>
-        <div className="flex gap-3 mx-2">
-          <input
-            className="border border-gray-400 rounded-full p-2 pr-[100px] pl-5 "
-            type="text"
-            onChange={(e) => setInput(e.target.value)}
-            value={input}
-            placeholder="Add a new task..."
-          />
-          <button
-            onClick={handleAddTask}
-            className="bg-teal-300 border-black rounded-full px-8 py-2"
-          >
-            ADD
-          </button>
+    <div className="bg-purple-300 h-screen p-10">
+      <div className="h-[70vh] bg-white border-purple-600 border-2   rounded-2xl w-[430px] m-auto">
+        <h1 className="font-bold text-2xl text-center p-4">To-Do List</h1>
+        <div className="w-[90%] m-auto">
+          <div className="flex gap-3 text-center">
+            <input onKeyDown={(e)=>{if(e.key==='Enter') handleAddTask()}}
+              className="border border-gray-400 rounded-full p-2 pr-[80px] pl-6 "
+              type="text"
+              onChange={(e) => setInput(e.target.value)}
+              value={input}
+              placeholder="Add a new task..."
+            />
+            <button
+              onClick={handleAddTask}
+              className="bg-purple-300 border-purple-600 border-2  rounded-full px-7 py-2"
+            >
+              ADD
+            </button>
+          </div>
+          {tasks
+            ? tasks.map((task, index) => (
+                <div className="flex gap-5 p-2">
+                  <input type="checkbox" />
+                  <p>{task}</p>
+                  <button onClick={() => handleEdit(index)}>Edit</button>
+                  <button onClick={() => handleDelete(index)}>Delete</button>
+                </div>
+              ))
+            : null}
         </div>
-        {tasks
-          ? tasks.map((task) => (
-              <div className="flex gap-5">
-                <input type="checkbox" />
-                <p>{task}</p>
-                <button>Edit</button>
-                <button>Delete</button>
-              </div>
-            ))
-          : null}
       </div>
     </div>
   );
